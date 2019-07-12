365dd1f5b42e48c77c2f311ca51dcbb113b5f7c6
Adam Kleczewski <adam@alkymi.io>
2019-07-12 15:25:52 -0400

    add overlap suppression

---
+++ overlap_suppression.py
@@ -0,0 +1,35 @@
+from __future__ import absolute_import
+
+import numpy as np
+import torch
+
+
+def overlap_suppression(dets):
+    dets = dets.numpy()
+    x1 = dets[:, 0]
+    y1 = dets[:, 1]
+    x2 = dets[:, 2]
+    y2 = dets[:, 3]
+    scores = dets[:, 4]
+
+    order = scores.argsort()
+
+    keep = []
+    while order.size > 0:
+        i = order.item(0)
+        keep.append(i)
+        xx1 = np.maximum(x1[i], x1[order[1:]])
+        yy1 = np.maximum(y1[i], y1[order[1:]])
+        xx2 = np.minimum(x2[i], x2[order[1:]])
+        yy2 = np.minimum(y2[i], y2[order[1:]])
+
+        w = np.maximum(0.0, xx2 - xx1 + 1)
+        h = np.maximum(0.0, yy2 - yy1 + 1)
+        inter = w * h
+
+        idxs = np.where(inter == 0.0)[0]
+        order = order[idxs + 1]
+
+    return torch.IntTensor(keep)
+
+
