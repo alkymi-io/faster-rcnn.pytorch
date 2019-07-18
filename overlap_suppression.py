f4462c75727ea56b766642e6e68451d41c68b9fb
Adam Kleczewski <adam@alkymi.io>
2019-07-18 01:52:15 -0400

    fix overlap suppression bugs

--- overlap_suppression.py
+++ overlap_suppression.py
@@ -17,34 +17,39 @@ def overlap_suppression(dets, thresh):
     y1 = dets[:, 1]
     x2 = dets[:, 2]
     y2 = dets[:, 3]
-    scores = dets[:, 4]
+    confidence = dets[:, 4:].max(axis=1)
     areas = (x2 - x1 + 1) * (y2 - y1 + 1)
 
-    order = scores.argsort()
+    # Sort boxes by decreasing confidence
+    order = confidence.argsort()[::-1]
 
     keep = []
     while order.size > 0:
-        # Keep the head of the list of confidence sorted boxes
+        # Add the most confident box to a list of boxes to be kept
         i = order.item(0)
         keep.append(i)
 
+        # Calculate the area of overlap between the most confident box and all
+        # the others
         xx1 = np.maximum(x1[i], x1[order[1:]])
         yy1 = np.maximum(y1[i], y1[order[1:]])
         xx2 = np.minimum(x2[i], x2[order[1:]])
         yy2 = np.minimum(y2[i], y2[order[1:]])
-
-        # Calculate the area of overlap
         w = np.maximum(0.0, xx2 - xx1 + 1)
         h = np.maximum(0.0, yy2 - yy1 + 1)
         inter = w * h
 
         # Calculate the fractional overlap relative to the smaller area
-        ovr = inter / np.minimum(areas[i], areas[order[1:]])
+        ovr = inter / np.minimum(areas[order[0]], areas[order[1:]])
 
-        idxs = np.where(ovr < thresh)[0]
+        # Get the indices where the fractional overlap does not exceed the
+        # threshold. Shift the indices to account for the most confident box.
+        idxs = np.where(ovr < thresh)[0] + 1
 
-        # Pop off the head
-        order = order[idxs + 1]
+        # Filter the bounding boxes. This keeps only boxes that do not
+        # significantly overlap with the most confidant box. This process
+        # always removes the most confidant box.
+        order = order[idxs]
 
     return torch.IntTensor(keep)
 
